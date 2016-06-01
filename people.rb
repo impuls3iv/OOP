12bce2b39e5e5114f4697bf7dee856b4c8b17917
Sam <sammy.unterman@gmail.com>
2016-06-01 15:52:09 -0400

    finished people

--- people.rb
+++ people.rb
@@ -1,15 +1,30 @@
-class Student
+class Person
+  attr_accessor :name
+  def greet
+    "Hi my name is #{name}"
+  end
+end
+
+class Student < Person
   def learn
     puts "I get it!"
   end
 end
 
-class Instructor
+class Instructor < Person
   def teach
     "Everything in ruby is an object"
   end
 end
 
-
-stud = Student.new
-stud.learn
+# bob = Instructor.new
+# bob.name = "bob"
+# puts bob.name
+# puts bob.teach
+chris = Instructor.new
+chris.name = "Chris"
+puts chris.greet
+christina = Student.new
+puts christina.greet
+puts chris.teach
+puts christina.learn
